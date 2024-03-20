ebdc044435254d2c16a3cd1410e8db8c1ab3cc5c
Marcus <marcus.nilsson10@hotmail.com>
2024-03-20 12:13:26 +0100

    Added another test in test_file_explorer for testing two blf files at once.

--- test_folder/test_file_explorer.py
+++ test_folder/test_file_explorer.py
@@ -21,6 +21,15 @@ class TestFileExplorer(unittest.TestCase):
         actual_file_name = self._sleeplogtool.file_explorer()
         expected_file_name = ["Sleeplogtool\\blf_testfiles\\Trace_BP11_Display_1_20240220_160600_20240221_062959_#08-1_LEM.blf"]
         self.assertEqual(expected_file_name, actual_file_name)
+
+    @patch("tkinter.filedialog.askopenfilenames", return_value=["SleepLogTool\\blf_testfiles\\Trace_BP11_Display_1_20240221_133700_20240221_133859_#08-1_LEM.blf",
+                                                                 "Sleeplogtool\\blf_testfiles\\Trace_BP11_Display_1_20240220_160600_20240221_062959_#08-1_LEM.blf"])
+    def test_file_explorer_2files(self, mock_input):
+        """ Tests if the opened file actualy returns the correct name. """
+        actual_file_name = self._sleeplogtool.file_explorer()
+        expected_file_name = ["SleepLogTool\\blf_testfiles\\Trace_BP11_Display_1_20240221_133700_20240221_133859_#08-1_LEM.blf",
+                              "Sleeplogtool\\blf_testfiles\\Trace_BP11_Display_1_20240220_160600_20240221_062959_#08-1_LEM.blf"]
+        self.assertEqual(expected_file_name, actual_file_name)
     
     @patch("tkinter.filedialog.askopenfilenames", return_value="")
     def test_file_explorer_no_file(self, mock_input):
