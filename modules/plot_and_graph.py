fa8a5d8ace1ff02a6b027260e2845852a4968bd5
Marcus Nilsson <marcus.nilsson10@hotmail.com>
2024-04-02 16:41:51 +0200

    Deleted all csv files because they aren't need now. Messed around with he plot but haven't come too far.

--- modules/plot_and_graph.py
+++ modules/plot_and_graph.py
@@ -1,6 +1,6 @@
 """ Imports. Numpy will be used. """
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 
 class PlotAndGraph():
@@ -28,7 +28,9 @@ class PlotAndGraph():
         first_time = df["Time"].min()
         y = df.Current.to_numpy()
         x = df.Time.to_numpy()
-        plt.plot((x - first_time) / 3600, y)
+        fig, ax = plt.subplots()
+        ax.plot((x - first_time) / 3600, y)
+        plt.axis([-0.000001, 0.000025, -1000, 71000])
         plt.xlabel("Time(h)", fontsize=15)
         plt.ylabel("Current(mA)", fontsize=15)
         plt.title("Sleeplog analysis", fontsize=24)
@@ -36,4 +38,39 @@ class PlotAndGraph():
         plt.grid()
         manager = plt.get_current_fig_manager()
         manager.window.state('zoomed')
+
+        # Create a draggable point
+        point, = ax.plot(0, np.sin(0), 'ro')  # Starting point at x=3
+
+        # Create a text annotation for displaying coordinates
+        text = ax.text(0, 0, 'hej', va='bottom')
+
+        def update_point(event):
+            if event.inaxes == ax:
+                x_mouse = event.xdata
+
+                # First x_mouse value is subtracted from all x elements.
+                # Then np.abs() makes converts all results to absolutes.
+                # Then np.argmin() takes the smallest value and returns the index.
+                # This index is then used to get the the value from the x and y arrays.
+                npabs = np.abs(x - x_mouse)
+                idx = np.argmin(np.abs(x - x_mouse))
+                print(idx)
+                print(x[6])
+                x_closest = x[idx]
+                y_closest = y[idx]
+
+                # Update the position of the draggable point
+                point.set_xdata(x_closest)
+                point.set_ydata(y_closest)
+
+                # Update the text annotation with coordinates
+                text.set_text(f'({x_closest:.2f}, {y_closest:.2f})')
+                text.set_position((x_closest, y_closest))
+                text.set_va('bottom')
+
+                fig.canvas.draw_idle()
+
+        fig.canvas.mpl_connect('motion_notify_event', update_point)
+
         plt.show()
