925a3884d6418fc53e20ed634abf6553038e5bbf
Marcus Nilsson <marcus.nilsson10@hotmail.com>
2024-04-03 14:18:15 +0200

    Got the plot to work like I want it to with the red dot following the line. I'm not finished though!

--- modules/plot_and_graph.py
+++ modules/plot_and_graph.py
@@ -29,9 +29,9 @@ class PlotAndGraph():
         y = df.Current.to_numpy()
         x = df.Time.to_numpy()
         fig, ax = plt.subplots()
-        ax.plot((x - first_time) / 3600, y)
-        plt.axis([-0.000001, 0.000025, -1000, 71000])
-        plt.xlabel("Time(h)", fontsize=15)
+        ax.plot((x - first_time), y)
+        #plt.axis([-0.000001, 0.01, -1000, 71000])
+        plt.xlabel("Time(0,01s)", fontsize=15)
         plt.ylabel("Current(mA)", fontsize=15)
         plt.title("Sleeplog analysis", fontsize=24)
         plt.subplots_adjust(left=0.05, bottom=0.06, right=0.97, top=0.955, wspace=None, hspace=None)
@@ -40,10 +40,13 @@ class PlotAndGraph():
         manager.window.state('zoomed')
 
         # Create a draggable point
-        point, = ax.plot(0, np.sin(0), 'ro')  # Starting point at x=3
+        point, = ax.plot(0, np.sin(0), 'ro')  # Starting point at x=0, y=0
 
         # Create a text annotation for displaying coordinates
         text = ax.text(0, 0, 'hej', va='bottom')
+
+        len_x = len(x)
+        print(len_x)
 
         def update_point(event):
             if event.inaxes == ax:
@@ -53,11 +56,14 @@ class PlotAndGraph():
                 # Then np.abs() makes converts all results to absolutes.
                 # Then np.argmin() takes the smallest value and returns the index.
                 # This index is then used to get the the value from the x and y arrays.
-                npabs = np.abs(x - x_mouse)
-                idx = np.argmin(np.abs(x - x_mouse))
-                print(idx)
-                print(x[6])
-                x_closest = x[idx]
+                x_time_min = min(x)
+                plus_x_mouse = x_time_min + x_mouse
+                x_first = x - plus_x_mouse
+                x_second = np.abs(x - plus_x_mouse)
+                x_3 = np.argmin(np.abs(x - plus_x_mouse))
+                idx = np.argmin(np.abs(x - plus_x_mouse))
+                #x_closest gets the time from x index and minus time before x-cordinate 0.
+                x_closest = x[idx] - x_time_min
                 y_closest = y[idx]
 
                 # Update the position of the draggable point
@@ -65,7 +71,7 @@ class PlotAndGraph():
                 point.set_ydata(y_closest)
 
                 # Update the text annotation with coordinates
-                text.set_text(f'({x_closest:.2f}, {y_closest:.2f})')
+                text.set_text(f'({x_closest:.7f}, {y_closest:.2f})')
                 text.set_position((x_closest, y_closest))
                 text.set_va('bottom')
 
