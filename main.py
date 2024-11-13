9dfde7f51bd378cba933f3707651dea8055e9ef3
Marcus Nilsson <marcus.nilsson10@hotmail.com>
2024-11-13 13:22:27 +0100

    Have corected the code in main and main_menu.py according to pylint.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from modules.main_menu import Menu
+""" Module Starts the main menu of the program and handles the overhead logic. """
+
+from modules.main_menu import MainMenu
 
 def main():
     """ Main. Start of the program. """
     print("Please wait...")
-    main_menu = Menu()
+    main_menu = MainMenu()
     main_menu.main_window()
 
 if __name__ == '__main__':
--- modules/main_menu.py
+++ modules/main_menu.py
@@ -1,28 +1,31 @@
-from tkinter import *
-from tkinter import messagebox
+""" Tkinter is used to sturcture up the menu graphically and functionally. 
+    The other files are for handeling logic for different parts of the menu
+    and also the graph window. """
+
+from tkinter import Tk, messagebox, Label, LabelFrame, Entry, Button, OptionMenu, Frame, StringVar
+import tkinter as tk
 from modules.files_preperation import FilesPreparation
 from modules.time_menu import TimeMenu
 from modules.other_settings_menu import OtherSettingsMenu
 from modules.plot_lines import PlotLines
 from modules.plot_and_graph import PlotAndGraph
 
-class Menu(Tk):
+class MainMenu(Tk):
     """ Handles the design and functionality of the menu. """
 
     def __init__(self):
-        super().__init__()
         """ Initializes the class. """
+        super().__init__()
         self.initialize_vars()
         self.fp = FilesPreparation(self.show_warning)
         self.settings_frame = Frame(self)
-        self.settings_frame.grid(row=0, column=1, sticky=N)
+        self.settings_frame.grid(row=0, column=1, sticky=tk.N)
         self.rtm = TimeMenu(self.settings_frame)
         self.osm = OtherSettingsMenu(self.settings_frame)
         self.pag = PlotAndGraph()
         self.browse_frame_create()
         self.analyze_cancel_frame_create()
         self.plot_line_create()
-        
 
         # Prepares the frame with lists to be filled.
         self.decide_bus.append(None)
@@ -34,6 +37,7 @@ class Menu(Tk):
 
 
     def initialize_vars(self):
+        """ Initializes the variables for the class. """
         self.toggle_buttons = []
         self.file_path_del_buttons = []
         self.x = 0
@@ -51,34 +55,45 @@ class Menu(Tk):
     def browse_frame_create(self):
         """ Creates the browse frame and adds all of it's contents. """
         self.browse_frame = LabelFrame(self, text="Choose blf/asc file(s)", padx=10, pady=5)
-        self.browse_frame.grid(row=0, rowspan=2, column=0, pady=10, padx=10, sticky=N)
+        self.browse_frame.grid(row=0, rowspan=2, column=0, pady=10, padx=10, sticky=tk.N)
 
         # Entry and button to add a file manually by entering path to it.
         self.browse_field = Entry(self.browse_frame, width=140, borderwidth=5)
         self.browse_field.grid(row=0, column=0, columnspan=6, padx=(0, 10), pady=(0, 10))
         add_button = Button(self.browse_frame, text="Add File", command=self.add_browse_field)
-        add_button.grid(row=0, column=6, columnspan=2, padx=(2, 3), sticky=N)
+        add_button.grid(row=0, column=6, columnspan=2, padx=(2, 3), sticky=tk.N)
 
         # Buttons to add files in different ways with the file_path_setup function.
-        choose_file_button = Button(self.browse_frame, text="Choose file(s)", command=lambda:self.add_file_path("file"))
-        choose_file_button.grid(row=1, column=0, sticky=W)
-        choose_folder_button = Button(self.browse_frame, text="Choose folder(s)", command=lambda:self.add_file_path("folder"))
+        choose_file_button = Button(self.browse_frame,
+                                    text="Choose file(s)",
+                                    command=lambda:self.add_file_path("file"))
+        choose_file_button.grid(row=1, column=0, sticky=tk.W)
+        choose_folder_button = Button(self.browse_frame,
+                                      text="Choose folder(s)",
+                                      command=lambda:self.add_file_path("folder"))
         choose_folder_button.grid(row=1, column=1, padx=10)
-        extract_LEM_button = Button(self.browse_frame, text="Extract LEM(s)", command=lambda:self.add_file_path("extract LEM"))
-        extract_LEM_button.grid(row=1, column=2,)
+        extract_lem_button = Button(self.browse_frame,
+                                    text="Extract LEM(s)",
+                                    command=lambda:self.add_file_path("extract LEM"))
+        extract_lem_button.grid(row=1, column=2,)
 
         # Button to add a parallel plot line and a drop down box to select wanted plot line.
-        add_plot_line_button = Button(self.browse_frame, text="Add Plot Line", command=self.plot_line_create)
+        add_plot_line_button = Button(self.browse_frame,
+                                      text="Add Plot Line",
+                                      command=self.plot_line_create)
         add_plot_line_button.grid(row=1, column=3, padx=(10, 30))
 
         # Updates the dropdownbox with the new line plot.
         drop_down_box_text = Label(self.browse_frame, text="Select Line Plot to add files to:")
-        drop_down_box_text.grid(row=1, column=4, sticky=E)
-        self.drop_down_box = OptionMenu(self.browse_frame, self.plot_line_select, *self.optionsmenu_list)
-        self.drop_down_box.grid(row=1, column=5, padx=(0, 160), sticky=W)
+        drop_down_box_text.grid(row=1, column=4, sticky=tk.E)
+        self.drop_down_box = OptionMenu(self.browse_frame,
+                                        self.plot_line_select,
+                                        *self.optionsmenu_list)
+        self.drop_down_box.grid(row=1, column=5, padx=(0, 160), sticky=tk.W)
 
-    
+
     def add_file_path(self, choice):
+        """ Adds a filepath to the plot line. """
         selected_plot_line = int(self.plot_line_select.get()[-1]) - 1
 
         files = self.fp.file_explorer(choice)
@@ -87,11 +102,12 @@ class Menu(Tk):
                                                                   selected_plot_line,
                                                                   self.decide_bus,
                                                                   self.file_path_del_buttons)
-        
+
         self.update_analyze_button()
 
-    
+
     def plot_line_create(self):
+        """ Adds a new plot line in main window. """
         plot_line_frame = PlotLines(self.browse_frame,
                                     self.plot_line_frames,
                                     self.analyze_button,
@@ -102,12 +118,16 @@ class Menu(Tk):
 
         # Delete button for specified line plot.
         # Uses lambda function to store correct number at the time of defining the delete button.
-        plot_line_del_button = Button(self.browse_frame,
-                                           text="X",
-                                           command=lambda i=len(self.plot_line_frames)-1: self.line_plot_del(i))
-        plot_line_del_button.grid(row=len(self.plot_line_frames)+1, column=7, padx=(10, 0), pady=20, sticky=NW)
-        self.plot_line_del_buttons.append(plot_line_del_button)
-    
+        plot_line_del_b=Button(self.browse_frame,
+                               text="X",
+                               command=lambda i=len(self.plot_line_frames)-1:self.line_plot_del(i))
+        plot_line_del_b.grid(row=len(self.plot_line_frames)+1,
+                                  column=7,
+                                  padx=(10, 0),
+                                  pady=20,
+                                  sticky=tk.NW)
+        self.plot_line_del_buttons.append(plot_line_del_b)
+
 
     def update_optionmenu(self, plot_line_frame, choice):
         """ Updates the optionsmenu for chosing plot. """
@@ -126,11 +146,12 @@ class Menu(Tk):
         for string in self.optionsmenu_list:
             menu.add_command(label=string,
                             command=lambda value=string: self.plot_line_select.set(value))
-        self.plot_line_select.set(text_to_set)                                 # Line that sets the text in drop down box!!
+        self.plot_line_select.set(text_to_set)
         self.update_drop_down_box()
 
 
     def line_plot_del(self, index):
+        """ Deletes selected plot line. """
         self.update_optionmenu(plot_line_frame=self.plot_line_frames[index], choice="delete")
         self.plot_line_frames[index].line_plot_del()
         del self.plot_line_frames[index]
@@ -142,6 +163,7 @@ class Menu(Tk):
 
 
     def update_drop_down_box(self):
+        """ Updates the drop down box containing the plot lines. """
         if self.plot_line_frames:
             self.drop_down_box.grid_configure(padx=(0, 160))
         else:
@@ -150,12 +172,14 @@ class Menu(Tk):
 
 
     def update_plot_line_frames(self):
+        """ Updates the rows for the plot lines. """
         for i, frame in enumerate(self.plot_line_frames):
             frame.config(text=f"Plot Line {i+1}")
             frame.grid_configure(row=i+2)
-    
+
 
     def update_plot_line_del_buttons(self):
+        """ Update the rows for the plot line delete buttons. """
         for i, button in enumerate(self.plot_line_del_buttons):
             button.grid_configure(row=i+2)
             button.config(command=lambda x=i: self.line_plot_del(x))
@@ -166,7 +190,11 @@ class Menu(Tk):
         file = self.browse_field.get()
         selected_plot_line = int(self.plot_line_select.get()[-1]) - 1
         if file:
-            self.plot_line_frames[selected_plot_line].file_path_setup(file, selected_plot_line, self.decide_bus, self.file_path_del_buttons, add="add")
+            self.plot_line_frames[selected_plot_line].file_path_setup(file,
+                                                                      selected_plot_line,
+                                                                      self.decide_bus,
+                                                                      self.file_path_del_buttons,
+                                                                      add="add")
 
         self.update_analyze_button()
 
@@ -174,10 +202,10 @@ class Menu(Tk):
     def analyze_cancel_frame_create(self):
         """ Creates the analyze/cancel frame and all of it's contents. """
         analyze_cancel_frame = Frame(self)
-        analyze_cancel_frame.grid(row=2, column=1, padx=5, sticky=SE)
+        analyze_cancel_frame.grid(row=2, column=1, padx=5, sticky=tk.SE)
         self.analyze_button = Button(analyze_cancel_frame,
                                      text="Analyze",
-                                     state=DISABLED,
+                                     state=tk.DISABLED,
                                      command=self.analyze_data,
                                      padx=15)
         self.analyze_button.grid(row=0, column=0, pady=10)
@@ -189,32 +217,33 @@ class Menu(Tk):
 
 
     def analyze_data(self):
+        """ Connected to the analyze button and start the analyze process of all the files. """
         dfs = self.fp.analyze_data(self.osm.get_choose_graph(),
                              self.plot_line_frames)
-        
+
         # Will skip set_df function if true and go to plotting directly.
         for df in dfs:
-            if df["Info"]["Skip"] == True:
+            if df["Info"]["Skip"]:
                 # MÅSTE FIXA PLOTTING_GRAPH, PLOTS HAR BYTTS MOT DFS!!!!
                 self.pag.plotting_graph(dfs, self.rtm.time_unit_selected.get())
                 return
-            else:
-                remove_start_time, remove_end_time = self.rtm.set_df(df)
-                
-                # Takes the dataframe and calls the function to remove the specified time, then plots the graph.
-                df = self.fp.remove_time(df, remove_start_time, remove_end_time)
-        self.pag.plotting_graph(dfs, self.rtm.time_unit_selected.get())
+            remove_start_time, remove_end_time = self.rtm.set_df(df)
 
+            # Takes the dataframe and calls the function to remove the specified time,
+            # then plots the graph.
+            df = self.fp.remove_time(df, remove_start_time, remove_end_time)
+        self.pag.plotting_graph(dfs, self.rtm.time_unit_selected.get())
 
 
     def update_analyze_button(self):
         """ Updates the analyze button """
         for array in self.plot_line_frames:
             if array.file_path_array:
-                self.analyze_button["state"] = NORMAL
+                self.analyze_button["state"] = tk.NORMAL
                 return
-        self.analyze_button["state"] = DISABLED
+        self.analyze_button["state"] = tk.DISABLED
 
 
     def show_warning(self, warning_text):
+        """ Shows warning with a provided pop-up message. """
         messagebox.showwarning("warning", warning_text)
